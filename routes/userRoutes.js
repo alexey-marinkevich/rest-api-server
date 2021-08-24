eb08093e64abca6ad35e2c133036a7d34e1abee4
Alex Marinkevich <bitrate595@gmail.com>
2021-08-24 16:52:05 +0300

    Cleanup & setup packages

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const router = express.Router();
-const User = require('../mongodb/models/user');
+const User = require('../models/user');
 
 router.post('/', (req, res, next) => {
   const user = new User({
